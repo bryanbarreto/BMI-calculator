cefcae4f97b5148b20c661e8b5a86004c4f5b1c1
bryan barreto <bryan.barreto@icloud.com>
2020-08-22 16:20:31 -0300

    layout done

--- IMC Calc/ViewController.swift
+++ IMC Calc/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alturaText: UILabel!
+    @IBOutlet weak var pesoText: UILabel!
+    @IBOutlet weak var alturaSlider: UISlider!
+    @IBOutlet weak var pesoSlider: UISlider!
+    
+    @IBAction func alturaSliderChange(_ sender: UISlider) {
+        let altura = String.init(format: "%.2f", sender.value)
+        alturaText.text = "\(altura) m"
+    }
+    
+    @IBAction func pesoSliderChange(_ sender: UISlider) {
+        let peso = Int(sender.value)
+        pesoText.text = "\(peso) Kg"
+    }
+    
+    @IBAction func btnCalcular(_ sender: UIButton) {
+        let altura:Float = alturaSlider.value
+        let peso:Float = pesoSlider.value
+        
+        let bmi:Float = peso / (altura * altura)
+        
+        print(bmi)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
